7b78fac0b54b0e464c9f9d04d5e1fd55e0df4dfb
D <htehran333@gmail.com>
2024-10-04 14:45:18 -0600

    update

--- src/comps/Blog/Blog_Side.js
+++ src/comps/Blog/Blog_Side.js
@@ -6,7 +6,7 @@ const Blog_Side = ({ author }) => {
   const latestBlogs = blogs.slice(-3).reverse();
 
   return (
-    <aside className="grid gap-y-5 max-lg:mt-12">
+    <aside className="flex flex-col gap-y-5 max-lg:mt-12">
       <section className="mac-border bg-blue-400 py-2 flex-seperate">
         <span className="text-slate-100">نویسنده :</span>
         <h5 className="text-slate-50">{author}</h5>
