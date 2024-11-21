923a5638b2dcaca86ff704389e5a2065a97be8ee
D <htehran333@gmail.com>
2024-11-21 14:12:03 -0500

    FAQ schema placed

--- src/comps/Commons/FAQ.js
+++ src/comps/Commons/FAQ.js
@@ -5,8 +5,12 @@ const FAQ = ({ questions, className = "" }) => {
   return (
     <section className={`grid gap-y-3 ${className}`}>
       <h2 className="title mb-3">ممکنه از خودت بپرسی: </h2>
-      {questions.map((item, index) => {
-        return <Accordion key={index} question={item} />;
+      {questions.mainEntity.map((item, index) => {
+        const question = {
+          quiz: item.name,
+          answer: item.acceptedAnswer.text,
+        };
+        return <Accordion key={index} question={question} />;
       })}
     </section>
   );
